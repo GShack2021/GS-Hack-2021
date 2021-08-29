4e2f178b569878cbdaa76b8f5aad1f363ad08d29
Quinn Cheong <quinncheong@hotmail.com>
2021-08-29 11:08:08 +0800

    Add screens

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'no-use-before-define': 0,
     'react/style-prop-object': 0,
+    'prettier/prettier': 0,
   },
   globals: {
     __DEV__: 'readonly',
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
 
-import { Main } from './navigation/Main';
+// Screens and views
+import ProfileScreen from './screens/Profile';
+import HomeScreen from './screens/Home';
+import LoansScreen from './screens/Loans'
+
+// import { Main } from './navigation/Main';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Main />
+        {/* <Main /> */}
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Loans" component={LoansScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
