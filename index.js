31bbde390134882cda2fce961f0a2c389f0357b0
Jonathan Tan <84013254+jonathantan1425@users.noreply.github.com>
2021-08-29 11:34:10 +0800

    added aws packages

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig)
 
 AppRegistry.registerComponent(appName, () => App)
